d1c52a0b6e9fd3ce749c44142b3162390e0b9ac2
NataliaKurash <kurashnataliiamariia@gmail.com>
2021-02-27 13:01:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import { foo, YES } from "./other";
+
+console.log(foo());
+console.log(YES);
---
+++ other.js
@@ -0,0 +1,8 @@
+export const PL = Math.PI;
+
+export function foo() {
+  const name = "foo";
+  return name + 170;
+}
+
+export const YES = true;
